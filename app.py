eccbdea8e739c693ba2a779e7fc932590f533950
janvi bhardwaj <janvi123456bhardwaj@gmail.com>
2024-11-17 22:05:59 +0530

    fix error

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ preprocessor = Pipeline(steps=[
 ])
 
 # read the training data
-train = pd.read_csv(r"C:\Users\as\OneDrive\Desktop\flight price prediction\data\train.csv")
+train = pd.read_csv("train.csv")
 X_train = train.drop(columns="price")
 y_train = train.price.copy()
 
